f5a25f65b07f5bd29e059ad0c8d2a3ef21d5966f
Felipe Dornelas <felipegabriel123dornelas@gmail.com>
2025-06-08 23:18:18 -0300

    corrigindo cliente repository

--- cliente-teste-modelo-main/projeto_cliente_teste/src/test/java/com/iftm/client/repositories/ClientRepositoryTest.java
+++ cliente-teste-modelo-main/projeto_cliente_teste/src/test/java/com/iftm/client/repositories/ClientRepositoryTest.java
@@ -1,46 +1,46 @@
-// package com.iftm.client.repositories;
+package com.iftm.client.repositories;
 
-// import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-// import com.iftm.client.entities.Client;
+import com.iftm.client.entities.Client;
 
-// @DataJpaTest
-// public class ClientRepositoryTest {
+@DataJpaTest
+public class ClientRepositoryTest {
 
-//     @Autowired
-//     private ClientRepository repositorio;
+    @Autowired
+    private ClientRepository repositorio;
 
-//     @Test
-//     public void testarBuscarPorNomeExistenteDeNomeQueContemAPalavra() {
-//         String nomeEsperado = "Ado";
-//         List<Client> clientObtido = repositorio.buscarPorNomePalavra(nomeEsperado);
-//         assertFalse(clientObtido.isEmpty());
-//     }
+    @Test
+    public void testarBuscarPorNomeExistenteDeNomeQueContemAPalavra() {
+        String nomeEsperado = "Ado";
+        List<Client> clientObtido = repositorio.buscarPorNomePalavra(nomeEsperado);
+        assertFalse(clientObtido.isEmpty());
+    }
 
-//     @Test
-//     public void testarBuscarPorNomeNaoExistenteDeNomeQueContemAPalavra() {
-//         String nomeEsperado = "lipe";
-//         List<Client> clientObtido = repositorio.buscarPorNomePalavra(nomeEsperado);
-//         assertTrue(clientObtido.isEmpty());
-//     }
+    @Test
+    public void testarBuscarPorNomeNaoExistenteDeNomeQueContemAPalavra() {
+        String nomeEsperado = "lipe";
+        List<Client> clientObtido = repositorio.buscarPorNomePalavra(nomeEsperado);
+        assertTrue(clientObtido.isEmpty());
+    }
 
-//     @Test
-//     public void testarClientesQueEstejamAcimaDoSalarioMaximo() {
-//         double tetoRenda = 5000.00;
-//         List<Client> clientObtido = repositorio.encontrarPorRendaMaior(tetoRenda);
-//         assertFalse(clientObtido.isEmpty());
-//     }
+    @Test
+    public void testarClientesQueEstejamAcimaDoSalarioMaximo() {
+        double tetoRenda = 5000.00;
+        List<Client> clientObtido = repositorio.encontrarPorRendaMaior(tetoRenda);
+        assertFalse(clientObtido.isEmpty());
+    }
 
-//     @Test
-//     public void testarClientesQueEstejamAbaixoDoSalarioMinimo() {
-//         double tetoRenda = 2000.00;
-//         List<Client> clientObtido = repositorio.encontrarPorRendaMenor(tetoRenda);
-//         assertFalse(clientObtido.isEmpty());
-//     }
-// }
+    @Test
+    public void testarClientesQueEstejamAbaixoDoSalarioMinimo() {
+        double tetoRenda = 2000.00;
+        List<Client> clientObtido = repositorio.encontrarPorRendaMenor(tetoRenda);
+        assertFalse(clientObtido.isEmpty());
+    }
+}
